4efc36cc837ab69d560e09be4249af6b66912cc4
AlekseiNime <mamagold1213@gmail.com>
2024-11-18 19:26:56 +0200

    Z2

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("a= ");
+            int a = int.Parse(Console.ReadLine());
+            Console.Write("b= ");
+            int b = int.Parse(Console.ReadLine());
+            int result = Sum(a, b);
+            Console.WriteLine(result);
+            Console.ReadKey();
+        }
+        static int Sum(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}

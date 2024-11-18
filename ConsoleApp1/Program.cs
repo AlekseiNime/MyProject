8ffda0cf60d78f9304694cca1e7b0dba2448dcbe
AlekseiNime <mamagold1213@gmail.com>
2024-11-18 19:22:21 +0200

    Z1

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Hello();
+            Bye();
+            Console.ReadKey();
+        }
+
+        static void Hello()
+        {
+            Console.WriteLine("Привет");
+        }
+        static void Bye()
+        {
+            Console.WriteLine("Пока");
+        }
+    }
+}

debb50c9712520c9c1c2cee8bfca6aafc791ba3c
AlekseiNime <mamagold1213@gmail.com>
2024-11-18 19:31:45 +0200

    Z3

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    internal class Program
+    {
+        
+
+        static void Main()
+        {
+            Console.Write("Введите значение a: ");
+            double a = Convert.ToDouble(Console.ReadLine());
+
+            double b = Calculate(a);
+
+            Console.WriteLine($"Значение функции для a = {a} равно b = {b}");
+            Console.ReadKey();
+        }
+        static double Calculate(double a)
+        {
+            return 12 * a * a + 7 * a - 6;
+        }
+    }
+}
